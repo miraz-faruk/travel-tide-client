f7d59004b5db6a2561cec0bf34204c8cb92eb43c
mirazfaruk <mirazfaruk@gmail.com>
2024-09-04 21:22:19 +0600

    first commit with added slider

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA9AeKVmvefJJhqyiG-W_ywR9AiUPNFUOE",
+    authDomain: "travel-tide-33354.firebaseapp.com",
+    projectId: "travel-tide-33354",
+    storageBucket: "travel-tide-33354.appspot.com",
+    messagingSenderId: "410074138968",
+    appId: "1:410074138968:web:0e0dc6b313be554f56fd2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Slider from "../Shared/Slider/Slider";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Slider></Slider>
+            <h2 className="text-3xl font-poppins">This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
